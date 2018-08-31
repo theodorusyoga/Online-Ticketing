66220622e87975a30f60f315d3d73820802bc215
Theodorus Yoga <theoyoga@outlook.com>
2018-08-31 09:16:57 +0700

    Initial commit with header jumbotron

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,15 @@
+@extends('master')
+
+@section('content')
+<div class="jumbotron header-jumbotron">
+    <div class="header-right ml-auto">
+        <img class="header-right-image" src="/img/earth.png" />
+    </div>
+    <div class="header-center">
+        <img class="header-center-gradient" src="/img/its-time-gradient.png"/>
+    </div>
+    <div class="header-bottom">
+        <img class="header-bottom-image" src="/img/buildings.png" />
+    </div>
+</div>
+@endsection
---
+++ resources/views/master.blade.php
@@ -0,0 +1,44 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Global UPR</title>
+        @component('include')
+        @endcomponent
+    </head>
+    <body>
+        <nav class="upr-custom-navbar navbar navbar-expand-md fixed-top">
+            <a class="navbar-brand" href="#">
+                <img class="upr-logo-navbar" src="/img/logo-top.png" alt="Global UPR 2018" />
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#upr-navbar" aria-controls="upr-navbar" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="upr-navbar">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Donation</a>
+                    </li>
+                    <li class="nav-item">
+                            <a class="nav-link" href="#">Join As Volunteer</a>
+                    </li>
+                    <li class="nav-item">
+                            <a class="nav-link" href="#">Register</a>
+                    </li>
+                </ul>
+            </div>
+            </nav>
+        <div class="content">
+            @yield('content')
+        </div>
+    </body>
+</html>

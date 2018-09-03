7abc32cf8346ab950e41806d2b055fe0b8d2bab7
Theodorus Yoga <theoyoga@outlook.com>
2018-09-03 16:16:06 +0700

    Updated user and password table. Youtube updated

--- resources/views/dashboard/venue.blade.php
+++ resources/views/dashboard/venue.blade.php
@@ -6,7 +6,7 @@
             Ham rump pork belly flank. Shankle turducken ham hock, alcatra t-bone brisket jowl chicken pork belly jerky tongue tri-tip pancetta.
         </p>
         <div class="video">
-                <iframe src="https://www.youtube.com/embed/S-YvVJLm33E" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+                <iframe src="https://www.youtube.com/embed/sCnZo2VAVm8" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
         </div>
         <p>
                 Ham rump pork belly flank. Shankle turducken ham hock, alcatra t-bone brisket jowl chicken pork belly jerky tongue tri-tip pancetta. Tenderloin flank pork, bresaola ball tip turkey short loin prosciutto cupim jowl venison ham pork belly t-bone. Capicola pig turkey frankfurter, rump shoulder prosciutto ground round doner. Alcatra andouille tri-tip kielbasa filet mignon pancetta brisket flank doner ground round frankfurter.

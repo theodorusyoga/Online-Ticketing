53be1b0247202a2aaf665f78159ff3be4629d176
kalibani <kautzar.alibani@modalrakyat.id>
2018-09-24 14:20:01 +0700

    Update page register finish

--- resources/assets/js/components/RegisterFinish.vue
+++ resources/assets/js/components/RegisterFinish.vue
@@ -3,14 +3,12 @@
     <div class="row">
       <div class="col-md-12 register-finish-wrapper">
         <div class="register-finish">
-          <H1 class="detil-order text-center">Pemesanan Selesai</H1>
+          <H1 class="detil-order text-center">Transaksi Selesai</H1>
           <div class="wrapper-logo">
             <img width="100" height="100" src="/img/its-time-gradient.png" alt="Global UPR" />
           </div>
           <p>
-            Terima Kasih telah melakukan pemesanan.
-            Batas waktu pembayaran pesanan Anda adalah 1 x 24 jam.<br>
-            Silakan lakukan pembayaran
+            Terima Kasih telah melakukan pembayaran.
           </p>
         </div>
       </div>

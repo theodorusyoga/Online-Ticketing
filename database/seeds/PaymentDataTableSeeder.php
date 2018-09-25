6dce5e247618f624e6eaeb00a4beae82a788962d
Theodorus <theoyoga@outlook.com>
2018-09-25 09:44:24 +0700

    Added payment notification skeleton. not yet working

--- database/seeds/PaymentDataTableSeeder.php
+++ database/seeds/PaymentDataTableSeeder.php
@@ -14,21 +14,31 @@ class PaymentDataTableSeeder extends Seeder
     {
         $payment1 = array(
             'user_id' => 'WGGBRZ0001',
-            'request_date' => Carbon::now('Asia/Jakarta'),
-            'expired_date' => Carbon::now('Asia/Jakarta')->addDays(2),
-            'ticket_price' => 200000,
-            'booking_code' => '123ABCD',
-            'total_price' => 200000,
+            'transaction_date' => '2018-09-25 00:41:12',
+            'status_code' => '200',
+            'message' => 'notification',
+            'signature_key' => 'ab6c0827ea84fff56dfd57f5f7b32513dd6da04e7cf52557f59c147fa6d6e033e32c2b8c419e837f6e204a23e82b2452ea9a48e207d1e470748ea06a2fe61a08',
+            'payment_type' => 'credit_card',
+            'masked_card'=> '481111-1114',
+            'gross_amount' => 40000000.00,
+            'channel_response_message' => 'Approved',
+            'bank' => 'mandiri',
+            'approval_code' => '1537810872811',
             'created_at' => '2018-09-02 00:00:00',
             'updated_at' => '2018-09-02 00:00:00'
         );
         $payment2 = array(
             'user_id' => 'WGGGLD0001',
-            'request_date' => Carbon::now('Asia/Jakarta'),
-            'expired_date' => Carbon::now('Asia/Jakarta')->addDays(2),
-            'ticket_price' => 300000,
-            'booking_code' => '456DEF',
-            'total_price' => 600000,
+            'transaction_date' => '2018-09-25 00:41:12',
+            'status_code' => '200',
+            'message' => 'notification',
+            'signature_key' => 'ab6c0827ea84fff56dfd57f5f7b32513dd6da04e7cf52557f59c147fa6d6e033e32c2b8c419e837f6e204a23e82b2452ea9a48e207d1e470748ea06a2fe61a08',
+            'payment_type' => 'credit_card',
+            'masked_card'=> '481111-1114',
+            'gross_amount' => 40000000.00,
+            'channel_response_message' => 'Approved',
+            'bank' => 'mandiri',
+            'approval_code' => '1537810872811',
             'created_at' => '2018-09-02 00:00:00',
             'updated_at' => '2018-09-02 00:00:00'
         );
